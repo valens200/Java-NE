16ca0f58d739974727b00c693cd66a85c4560208
valens200 <valensniyonsenga2003@gmail.com>
2024-05-14 20:59:54 +0200

    [init]:  template

--- src/main/java/com/templates/valens/v1/models/enums/ResultResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.templates.valens.v1.models.enums;
-
-import com.templates.valens.v1.models.Candidate;
-import com.templates.valens.v1.models.Position;
-
-public class ResultResponseDTO {
-    private Candidate candidate;
-    private Position position;
-}
